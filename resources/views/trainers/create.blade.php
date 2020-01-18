432a45e1b2df23914d77e5941bf3e0040889184b
Miguel <miguelangelzk0405@gmial.com>
2020-01-18 13:33:09 -0500

    agregando controlador (resourses) vista y rutas para la seccion de trainers - creando plantillas con blade

---
+++ resources/views/trainers/create.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+
+@section('title', 'trainers create')
+    
+
+
+<div class="container">
+    <div class="form-group">
+    <label for="">Nombre</label>
+    <input type="text" class"form-control">
+    </div>
+    <button type="submit" class="btn-primary">Guardar</button>
+    </div>
+</body>
