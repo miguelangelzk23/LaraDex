eee1376939d138410897d756dd5e50a75e9fcd8d
Miguel <miguelangelzk0405@gmial.com>
2020-01-29 16:34:07 -0500

    mejorando vista index de entrenadores

--- resources/views/trainers/index.blade.php
+++ resources/views/trainers/index.blade.php
@@ -8,8 +8,8 @@
 @foreach ( $trainers as $trainer)
 <div class="col-sm">
 
-    <div class="card" style="width: 18rem;"> 
-      <img class="card-img-top" src="images/{{$trainer->avatar}}">
+    <div class="card text-center" style="width: 18rem; margin-top: 70px" > 
+      <img style="height: 100px;width: 100px;background-color: #EFEFEF;margin: 20px" class="card-img-top rounded-circle mx-auto display-block" src="images/{{$trainer->avatar}}">
     <div class="card-body">
       <h5 class="card-title">{{$trainer->nombre}}</h5>
       <p class="card-text">{{$trainer->descripcion}}</p>
